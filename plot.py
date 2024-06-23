366a5c8fe8b57df7c15a0f66db04f80000d54857
Adam <aedamjung@gmail.com>
2024-06-23 04:35:12 +0000

    added comments

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Arrow
 
+# draw the robot 
 def plot_robot(robot, ax):
     x, y, theta = robot.get_position()
     arrow = Arrow(x, y, 0.5 * np.cos(theta), 0.5 * np.sin(theta), width=0.3)
